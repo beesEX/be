da1cd4cdd075e4de9ebd0df78ebfdc91e1a19f82
Vu Duc Tung <tunggad.vu@googlemail.com>
2018-07-01 11:57:43 +0200

    fixed #1: [OrderManagment]: create Order Model in src/resources/order/order.schema.js

---
+++ src/resources/order/order.schema.js
@@ -0,0 +1,17 @@
+const Joi = require('joi');
+const { CURRENCY_SYMBOLS } = require('app.constants');
+
+const orderSchema = {
+  _id: Joi.string(),
+  type: Joi.string().allow(['LIMIT', 'MARKET', 'STOP']).required(),
+  side: Joi.string().allow(['BUY', 'SELL']).required(),
+  currency: Joi.string().allow(CURRENCY_SYMBOLS).required(),
+  baseCurrency: Joi.string().allow(CURRENCY_SYMBOLS).required(),
+  limitPrice: Joi.number().positive(),
+  quantity: Joi.number().positive().required(),
+  filledQuantity: Joi.number().min(0),
+  status: Joi.string().allow(['PLACED', 'PARTIALLY_FILLED', 'FILLED', 'CANCELED', 'EXPIRED']),
+  userId: Joi.string(),
+};
+
+module.exports = obj => Joi.validate(obj, orderSchema, { allowUnknown: true });
