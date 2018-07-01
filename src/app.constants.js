5441ad5f33deb2d4b163172a4bd59b489b9d9b3b
Vu Duc Tung <tunggad.vu@googlemail.com>
2018-07-01 11:53:42 +0200

    fixed #6: Find out why the db.js module does not work out of the box and fix it

--- src/app.constants.js
+++ src/app.constants.js
@@ -2,4 +2,5 @@ module.exports = {
   DATABASE_DOCUMENTS: {
     USERS: 'users',
   },
+  CURRENCY_SYMBOLS: ['BTC', 'BCH', 'ETH', 'XRP', 'EOS', 'LTC', 'XLM', 'ADA', 'USDT', 'MIOTA', 'NANO', 'ICX'],
 };
