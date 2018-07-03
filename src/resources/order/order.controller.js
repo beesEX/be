80536e3e2fc9a16f0689539e21dfac84410cd7ba
Vu Duc Tung <tunggad.vu@googlemail.com>
2018-07-03 10:27:39 +0200

    review works of vietanh for task #7, #9

--- src/resources/order/order.controller.js
+++ src/resources/order/order.controller.js
@@ -3,6 +3,7 @@ const { logger } = global;
 
 const orderService = require('./order.service');
 
+/* POST /order/place */
 exports.orderPlaceHandler = async (ctx, next) => {
   logger.info('order.controller.js: Receive POST ORDER request');
 
@@ -14,7 +15,7 @@ exports.orderPlaceHandler = async (ctx, next) => {
     limitPrice: ctx.request.body.limitPrice,
     quantity: ctx.request.body.quantity,
     filledQuantity: 0.0,
-    status: 'PLACED',
+    status: 'PLACED', // Hay tao mot array chua cac constants cho status trong orderSchema, va su dung nhung constants do, ko dung literal string value nhu the nay, very error-prone
     createdAt: new Date(),
     lastUpdatedAt: new Date(),
     userId: ctx.state.user._id.toString()
@@ -23,15 +24,18 @@ exports.orderPlaceHandler = async (ctx, next) => {
   ctx.body = await orderService.placeOrder(newOrderObj);
 };
 
-exports.orderUpdateHandler = async (ctx, next) => {
+/* POST /order/update */
+exports.orderUpdateHandler = async (ctx, next) => { // moi route handler nen co mot dong log in ra rang no da nhan dc request va parameter no can
   ctx.body = await orderService.updateOrder(ctx.request.body);
 };
 
-exports.orderCancelHandler = async (ctx, next) => {
+/* POST /order/cancel */
+exports.orderCancelHandler = async (ctx, next) => { // moi route handler nen co mot dong log in ra rang no da nhan dc request va parameter no can
   await orderService.cancelOrder(ctx.request.body.orderId, ctx.state.user._id.toString());
-  ctx.body = {};
+  ctx.body = {}; // tra ve http response code 200 co ma, viet the nay cung dc, nhung ko lam ro muc dich la minh chi muon tra ve ctx.status = 200;
 };
 
-exports.orderActiveHandler = async (ctx, next) => {
+/* GET /order/active */
+exports.orderActiveHandler = async (ctx, next) => { // moi route handler nen co mot dong log in ra rang no da nhan dc request va parameter no can
   ctx.body = await orderService.getActiveOrder(ctx.state.user._id.toString());
 };
--- src/resources/order/order.service.js
+++ src/resources/order/order.service.js
@@ -9,6 +9,11 @@ const service = db.createService(constants.DATABASE_DOCUMENTS.ORDERS, schema);
 
 module.exports = {
 
+  /**
+   * js function doc pls!!!
+   * @param newOrderObject
+   * @returns {Promise<*>}
+   */
   placeOrder : async (newOrderObject) => {
     service.create(newOrderObject);
     logger.info('order.service.js: placed order:',JSON.stringify(newOrderObject, null, 2));
@@ -16,11 +21,16 @@ module.exports = {
     return newOrderObject;
   },
 
+  /**
+   * js function doc pls!!!
+   * @param orderObject
+   * @returns {Promise<void>}
+   */
   updateOrder : async (orderObject) => {
-    const updatedOrder = await service.update({ _id: orderObject._id}, (doc) => {
+    const updatedOrder = await service.update({ _id: orderObject._id}, (doc) => { // sao ko kiem tra userId cua order can update?
       doc.limitPrice = orderObject.limitPrice;
-      doc.filledQuantity = orderObject.filledQuantity;
-      doc.status = orderObject.status;
+      doc.filledQuantity = orderObject.filledQuantity; // ko update filledQuantity, service function nay chi de dung cho nguoi dung update, chi update dc limitPrice va quantity thoi. Trading Engine se co update function rieng de update fillQuantity
+      doc.status = orderObject.status; // ko update status, nguoi dung ko dc tu dong update status, Trading Engine se co function rieng de update status
       doc.lastUpdatedAt = new Date()
     });
     logger.info('order.service.js: updated order =', JSON.stringify(updatedOrder, null, 2));
@@ -28,21 +38,33 @@ module.exports = {
     return updatedOrder;
   },
 
+  /**
+   * js function doc pls!!!
+   * @param orderId
+   * @param userId
+   * @returns {Promise<void>}
+   */
   cancelOrder : async (orderId, userId) => {
     await service.update({ _id: orderId, userId: userId}, (doc) => {
-      doc.status = "CANCELED";
+      doc.status = "CANCELED"; // Hay tao mot array chua cac constants cho status trong orderSchema, va su dung nhung constants do, ko dung literal string value nhu the nay, very error-prone
       doc.lastUpdatedAt = new Date();
       logger.info('order.service.js: canceled order with id =', orderId);
     });
     return;
   },
 
+  /**
+   * js function doc pls!!!
+   * @param userId
+   * @returns {Promise<Array>}
+   */
   getActiveOrder : async (userId) => {
     const maxOrderNumber = 3000; // max number of documents per page
     let results = [];
 
     const activeStatus = ['PLACED', 'PARTIALLY_FILLED'];
 
+    // thuc hien nhung query optimizations nhu Ngoc Son noi o tren chat
     for(let k=0; k<activeStatus.length; ++k){
       let result = await service.find({userId: userId, status: activeStatus[k]}, {perPage: maxOrderNumber}); // page default = 0
       results = results.concat(result.results);
