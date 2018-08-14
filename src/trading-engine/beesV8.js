4aa91a778fadb3d4a0b9b1fc8bab81adc728817f
vietho <hovietanh.1991@gmail.com>
2018-08-14 10:04:35 +0200

    do not start zeroMQ from here anymore

--- src/trading-engine/beesV8.js
+++ src/trading-engine/beesV8.js
@@ -1,19 +1,17 @@
 const { fork } = require('child_process');
 const uuid = require('uuid/v4');
-const { EVENT_GET_AGGREGATED_STATE, EVENT_GET_ORDERBOOK_STATE } = require('./orderbook');
+const { EVENT_GET_AGGREGATED_STATE, EVENT_GET_ORDERBOOK_STATE, ORDER_BOOK_EVENT } = require('./orderbook.event');
 
 const { logger } = global;
 
-const config = require('../config');
+//const config = require('../config');
 
-const {open, publish, close} = require('../util/zeroMQpublisher');
+const zeroMQ = require('../util/zeroMQpublisher');
 
 async function sendMessage(message, topic) {
   return new Promise((resolve) => {
-    setTimeout(() => { // [Tung]: why timeout of 1000 millisecond?
-      publish(`${message}`, topic);
-      resolve();
-    }, 1000);
+    zeroMQ.publish(`${message}`, topic);
+    resolve();
   });
 }
 
@@ -34,35 +32,27 @@ class BeesV8 {
     logger.info(`BeesV8 for ${this.symbol} starts`);
 
     // start zero MQ
-    if (config.isDev) open(); // [Tung]: why not start zeroMQ in app.listen() function in app.js? And conditional starting of zeroMQ only for DEV is wrong too, what is with PROD?
+    //if (!config.isTest) open();
 
     this.orderbookChildProcess = fork('src/trading-engine/orderbook.js');
 
     this.orderbookChildProcess.on('message', (message) => {
       logger.info(`beesV8.js: receives message from orderboook-childprocess: ${JSON.stringify(message)}`);
 
-      /**
-       * [Tung]: events sent from orderbook-childprocess need to be handled in consistent way. In following if-block
-       * you just check type of 'ORDER_BOOK_EVENT', so what is with other event types: 'GET_AGGREGATED_STATE', 'GET_ORDERBOOK_STATE'?
-       * I know that the logic in this if-block is specific to 'ORDER_BOOK-EVENT', but at some abstract level the structure of
-       * handling logic must be consistent for all event types -> always checking type of events in if-else-if-blocks.
-       */
-      if (message.type === 'ORDER_BOOK_EVENT') { // [Tung]: define type constant for events where they were emitted in orderbook.js, and use that constant here
+      if (message.type === ORDER_BOOK_EVENT) {
         sendMessage(JSON.stringify(message), `Orderbook-${this.symbol}`).then(() => {
           logger.info(`beesV8.js: publishes orderbook event per zeroMQ to UI server: ${JSON.stringify(message)}`);
         });
       }
-
-      /**
-       * [Tung]: You don't check event type here, that means, you will do the same logic for all there event types sent from
-       * orderbook childprocess, which can't work for events of type 'ORDER_BOOK_EVENT', which do not have id field. Tt is
-       * accidentally the correct handling of 'ORDER_BOOK_EVENT', but as mentioned above, the structure of event handling logic
-       * for all three event types must be somehow consistent  -> always checking type of events in if-else-if-blocks.
-       */
-      const resolveFunction = this.mapOfIdAndResolveFunction[message.id];
-      if (resolveFunction) {
-        resolveFunction(message.state);
-        delete this.mapOfIdAndResolveFunction[message.id];
+      else if (message.type === EVENT_GET_ORDERBOOK_STATE || message.type === EVENT_GET_AGGREGATED_STATE) {
+        const resolveFunction = this.mapOfIdAndResolveFunction[message.id];
+        if (resolveFunction) {
+          resolveFunction(message.state);
+          delete this.mapOfIdAndResolveFunction[message.id];
+        }
+      }
+      else {
+        logger.error(`beesV8.js: unknown message type ${JSON.stringify(message.type)}`);
       }
     });
   }
@@ -133,7 +123,7 @@ class BeesV8 {
    */
   stop() {
     this.orderbookChildProcess.kill();
-    if (config.isDev) close();
+    //if (!config.isTest) close();
   }
 
 }
