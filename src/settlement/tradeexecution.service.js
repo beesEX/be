7886a0d3bef08366daa47a9640ef842fa699d372
vietho <hovietanh.1991@gmail.com>
2018-08-19 15:41:11 +0200

    TODO: call transaction service to update wealth

--- src/settlement/tradeexecution.service.js
+++ src/settlement/tradeexecution.service.js
@@ -1,6 +1,10 @@
+const config = require('../config');
 const {logger} = global;
 
+const {REASON_OBJECT_TYPE, ORDER_BOOK_EVENT} = require('../trading-engine/orderbook.event');
+
 const Transaction = require('../wealth-management/transaction.service');
+const OrderService = require('../resources/order/order.service');
 
 module.exports = {
   executeTrades: async (orderbookEvent) => {
@@ -15,5 +19,37 @@ module.exports = {
 
 
     * */
+
+    orderbookEvent = JSON.parse(orderbookEvent);
+
+    logger.info(`tradeexecution.service.js executeTrades(): received orderbookEvent = ${JSON.stringify(orderbookEvent)}`);
+
+    if (!orderbookEvent || !orderbookEvent.reason || orderbookEvent.type !== ORDER_BOOK_EVENT) {
+      logger.info(`tradeexecution.service.js executeTrades(): ERROR: unexpected type`);
+      return false;
+    }
+
+    const reasonObj = orderbookEvent.reason;
+    const matchList = orderbookEvent.matches;
+    const isReasonObjFilledCompletely = orderbookEvent.filledCompletely;
+
+    if (orderbookEvent.reason.type === REASON_OBJECT_TYPE.CANCELED) {
+      // need user Id in each match object and reason object
+      //await Transaction.release()
+    }
+
+    for (let i = 0; i < matchList.length; i += 1) {
+      const status = await OrderService.updateOrdersbyMatch(reasonObj, matchList[i], i === matchList.length - 1 && isReasonObjFilledCompletely);
+      if (!status) {
+        logger.error(`tradeexecution.service.js executeTrades(): ----- ERROR: ------ RACE CONDITION ------`);
+      }
+      else {
+        // call transaction service
+
+      }
+    }
+    return true;
+
   },
+
 };
