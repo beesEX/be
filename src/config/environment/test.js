94e492e32782e99bab591db3852d7c84652b2ff1
Chumi <Chumi@Trangs-MacBook-Pro.local>
2018-08-21 22:02:44 +0200

    change db name for test env to beesEX-test

--- src/config/environment/test.js
+++ src/config/environment/test.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongo: {
-    connection: 'mongodb://localhost:27017/beesEX',
+    connection: 'mongodb://localhost:27017/beesEX-test',
   },
   zeroMQHost: 'tcp://127.0.0.1:42000'
 };
